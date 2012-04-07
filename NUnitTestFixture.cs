61022329add058ac8d97d1a7f352ad9d4e5e795d
M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>
2012-04-07 15:35:20 +0100

    Signed-off-by: M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>

---
+++ NUnitTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace $rootnamespace$
+{
+    [TestFixture]
+    public class $itemname$
+    {
+        [Test]
+        public void FailingTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
