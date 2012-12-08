2e6ae1909a1ed410fefcd2dd139b90f2232eda31
radleta <radleta@gmail.com>
2012-12-08 09:13:52 -0500

    Updated to be more generic and ready to use

--- NUnitTestFixture.cs
+++ NUnitTestFixture.cs
@@ -1,15 +1,23 @@
-﻿using NUnit.Framework;
-using Rhino.Mocks;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace $rootnamespace$
 {
+	/// <summary>
+	/// The text fixture for <see cref="$itemname$" />.
+	/// </summary>
     [TestFixture]
     public class $itemname$
     {
+		/// <summary>
+		/// Test for <see cref="$itemname$" />.
+		/// </summary>
         [Test]
-        public void FailingTest()
+        public void Test()
         {
-            Assert.Fail();
+            
         }
     }
 }
